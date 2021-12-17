b30d6624e31f4081a79c279627c73f2e3cf48db1
Ishan koradia <koradiaishan335@gmail.com>
2021-12-17 17:14:59 +0530

    modal for saving movies

--- client/src/components/SaveMovieModal.js
+++ client/src/components/SaveMovieModal.js
@@ -1,49 +1,71 @@
 import { React, useState, useEffect, useContext } from 'react'
 import PlaylistContext from '../context/playlistContext';
 import '../styles/saveMovieModal.css';
+import CurrentMovieContext from '../context/currentMovieContext';
 
-const Modal = ({show}) => {
+const Modal = ({show, setShow}) => {
+    //movie to save
+    const {currentMovie, setCurrentMovie} = useContext(CurrentMovieContext);
+
     const [checkedState, setCheckedState] = useState([]);
-
+    
+    //all playlists of user
     const {currentPlaylists, setCurrentPlaylists} = useContext(PlaylistContext);
+    
+    //save to these playlists
+    const [saveToPlaylists, setSaveToPlaylists] = useState([]);
 
     useEffect(() => {
-        setCurrentPlaylists([{name: 'recipe', _id: "1"}, {name: 'exercise', _id: "2"}])
-    }, [currentPlaylists])
+        //fetch current user's playlist from playlist context
+        setCheckedState(new Array(currentPlaylists.length).fill(false));
+    }, [show, setShow])
 
-    if(!show){
-        return null
+    function handleCheckboxChange(position) {
+        const updatedCheckedState = checkedState.map((item, index) =>
+            index === position ? !item : item
+        );
+
+        setCheckedState(updatedCheckedState);
     }
-  
 
-    const savePlaylists = []
+    function closeModal(){
+        setShow(false);
+    }
 
-    function handleCheckboxClick(e){
-        
+    function saveModal(){
+        setShow(false);//close the modal
+
+        //save this movie to all the playlists selected
+        setSaveToPlaylists(currentPlaylists.filter((item, index) => checkedState[index]));
+
+        //post request to save the movie to the playlists checked
     }
 
     return (
+        <div>{ show &&
         <div className="modal">
             <div className="modal-content">
                 <div className="modal-header">                   
-                    <p className="modal-title">Save video to...</p><button>x</button>               
+                    <p className="modal-title">Save video to...</p><button onClick={closeModal}>x</button>               
                 </div>
                 <div className="modal-body">
-                    {(currentPlaylists.length > 0) ? currentPlaylists.map(item => 
-                        <div><input 
+                    {(currentPlaylists.length > 0) ? currentPlaylists.map(({name, _id}, index) => 
+                        <div key={currentPlaylists[index]._id}><input 
                                 type="checkbox" 
-                                id={item._id} 
-                                name="recipe" 
-                                value="recipe"
-                                onClick={handleCheckboxClick} 
-                            /> {item.name}</div>) :
+                                //id={_id} 
+                                value={currentPlaylists[index]}
+                                checked={checkedState[index] || ''} //so that we dont get the warning of uncontrolled to controlled
+                                onChange={() => handleCheckboxChange(index)} 
+                            /> {name}</div>) :
                         <div>Please create a new playlist</div>
                     }
                 </div>
                 <div className="modal-footer">
-                    <button className="button" >Done</button>
+                    <button className="button" onClick={saveModal}>Save</button>
                 </div>
             </div>            
+        </div>
+                }
         </div>
     )
 }
