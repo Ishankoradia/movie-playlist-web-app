beaf79ebb00457332b3307040d3815a052b1e0a4
Ishan koradia <koradiaishan335@gmail.com>
2022-01-10 22:00:57 +0530

    Jwt auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,19 +2,22 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header("x-auth-token");
-    if (!token)
+    const token = req.header("x-access-token");
+    if (!token){
       return res
         .status(401)
-        .json({ msg: "No authentication token, authorization denied." });
+        .json({ success: false, error: "No authentication token, authorization denied." });
+    }      
 
     const verified = jwt.verify(token, process.env.APP_SECRET_KEY);
-    if (!verified)
+
+    if (!verified){
       return res
         .status(401)
-        .json({ msg: "Token verification failed, authorization denied." });
+        .json({ success: false, error: "Token verification failed, authorization denied." });
+    }       
 
-    req.user = verified.id;
+    req.user_id = verified.id;
     next();
   } catch (err) {
     res.status(500).json({ error: err.message });
